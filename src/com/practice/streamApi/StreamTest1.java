ca5127b80cef8ae084cd188166bb9bed17d25a82
Swarup Mandal <swarup.mandal@indusnet.co.in>
2022-05-25 13:05:10 +0530

    updated on 25-05-22

---
+++ src/com/practice/streamApi/StreamTest1.java
@@ -0,0 +1,12 @@
+package com.practice.streamApi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest1 {
+    public static void main(String[] args) {
+        List<Integer> list1= Arrays.asList(10,13,15,18,20);
+
+    }
+}
